e85bbaf31f8ee040331a209d624323e43bc24c18
RaizaLR <raizalongart@gmail.com>
2020-11-05 12:09:55 -0300

    agregue la funcion basica de cifrar y descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,51 @@
-const cipher = {
-  // ...
-};
+// const cipher = {
+//   // ...
+// };
 
-export default cipher;
+// export default cipher;
+
+const cipher = {
+
+    encode() {
+    let texto = document.getElementById("containerOne").value;
+
+    let textoCifrado = "";
+
+    for(let i= 0; i < texto.length; i++) {
+
+        let textoEnAscii = texto.charCodeAt(i);
+
+        let textoFormulado = (textoEnAscii - 65 + 1) %26 + 65;
+
+        let letrasCifradas = String.fromCharCode(textoFormulado);
+
+        textoCifrado= textoCifrado + letrasCifradas;
+     
+        }
+
+        console.log(textoCifrado);
+
+}
+,
+decode(){
+
+    let texto = document.getElementById("containerOne").value;
+
+    let textoDescifrado = "";
+
+    for(let i= 0; i < texto.length; i++) {
+
+        let textoEnAscii = texto.charCodeAt(i);
+
+        let textoFormulado = (textoEnAscii - 65 - 1) %26 + 65;
+
+        let letrasDescifradas = String.fromCharCode(textoFormulado);
+
+        textoDescifrado= textoDescifrado + letrasDescifradas;
+     
+        }
+
+        console.log(textoDescifrado);
+}
+
+}
